16da6ffa316ae8d745101bf6cb68dbe22840ec5d
Morgan Prior <morgan@amaras-tech.co.uk>
2016-04-23 11:36:46 +0100

    Cleaned up calls for all locations and removed exit, (using dry run)

--- transfer_progress.sh
+++ transfer_progress.sh
@@ -2,50 +2,34 @@
 
 LOCAL_FOLDER='/mnt/virtual/tank'
 COOPER='192.168.0.31'
-REMOTE_MACHINE=$COOPER
+REMOTE_FOLDER="$COOPER:/data/CooperMedia"
 
-    RSYNC_OPTIONS='-av --progress --ignore-errors --delete'
-    # Dry run
-    RSYNC_OPTIONS="$RSYNC_OPTIONS --dry-run"
-    RSYNC_EXCLUDE="--exclude '.Trashes'"
-    RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.Spotlight-V100'"
-    RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.DS_Store'"
-    RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '._.DS_Store'"
+RSYNC_OPTIONS='-av --progress --ignore-errors --delete'
+## Dry run
+RSYNC_OPTIONS="$RSYNC_OPTIONS --dry-run"
+
+RSYNC_EXCLUDE="--exclude '.Trashes'"
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.Spotlight-V100'"
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.DS_Store'"
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '._.DS_Store'"
     
-    ## Play Nice with BT Sync
-    RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.sync'"
-
+## Play Nice with BT Sync
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.sync'"
 
 
 echo "Backing up Movies"
 # requires sudo if using ssh to transfer data (ip:/local/path)
-#sudo rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE  \
-#$LOCAL_FOLDER/Movies \
-#$REMOTE_MACHINE:/data/CooperMedia
+eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/Movies $REMOTE_FOLDER
 
 echo "Backing up TV Shows"
 # requires sudo if using ssh to transfer data (ip:/local/path)
-eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/TV $REMOTE_MACHINE:/data/CooperMedia
-
-exit 0
+eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/TV $REMOTE_FOLDER
 
 echo "Backing up Audiobooks"
 # requires sudo if using ssh to transfer data (ip:/local/path)
-sudo rsync -av --progress --ignore-errors --delete --exclude '.Trashes'   \
---exclude '.Spotlight-V100' --exclude '.DS_Store' --exclude '._.DS_Store' \
-/mnt/virtual/tank/Audiobooks \
-$COOPER:/data/CooperMedia
+eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/Audiobooks $REMOTE_FOLDER
 
 echo "Backing up Music"
 # requires sudo if using ssh to transfer data (ip:/local/path)
-sudo rsync -av --progress --ignore-errors --delete --exclude '.Trashes'   \
---exclude '.Spotlight-V100' --exclude '.DS_Store' --exclude '._.DS_Store' \
-/mnt/virtual/tank/Music \
-$COOPER:/data/CooperMedia
-#$COOPER:/data/CooperMusic
+eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/Music $REMOTE_FOLDER
 
-
-
-#/mnt/virtual/tank/Music \
-#192.168.0.73:/data/CooperMusic 
-#/mnt/cooper/coopermusic
