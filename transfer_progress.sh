6eab5ef738732a2459b0e7a519b798a888e7c5c8
Morgan Prior <morgan@amaras-tech.co.uk>
2016-04-23 11:28:53 +0100

    dry run param

--- transfer_progress.sh
+++ transfer_progress.sh
@@ -5,6 +5,8 @@ COOPER='192.168.0.31'
 REMOTE_MACHINE=$COOPER
 
     RSYNC_OPTIONS='-av --progress --ignore-errors --delete'
+    # Dry run
+    RSYNC_OPTIONS="$RSYNC_OPTIONS --dry-run"
     RSYNC_EXCLUDE="--exclude '.Trashes'"
     RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.Spotlight-V100'"
     RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.DS_Store'"
@@ -23,7 +25,7 @@ echo "Backing up Movies"
 
 echo "Backing up TV Shows"
 # requires sudo if using ssh to transfer data (ip:/local/path)
-eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE --dry-run $LOCAL_FOLDER/TV $REMOTE_MACHINE:/data/CooperMedia
+eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/TV $REMOTE_MACHINE:/data/CooperMedia
 
 exit 0
 
