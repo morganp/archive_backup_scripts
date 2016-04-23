ed065d0cf6365a7b9a95af4fdac578d7a3810b8c
Morgan Prior <morgan@amaras-tech.co.uk>
2016-04-23 11:41:45 +0100

    Updated to current setup

--- transfer_tv.sh
+++ transfer_tv.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 # requires sudo if using ssh to transfer data (ip:/local/path)
-cmd="sudo rsync -av --ignore-errors --delete --exclude '.Trashes'   \
---exclude '.Spotlight-V100' --exclude '.DS_Store' --exclude '._.DS_Store' --exclude '.sync' \
-/mnt/virtual/tank/TV \
-192.168.0.31:/data/CooperMedia"
+LOCAL_FOLDER='/mnt/virtual/tank'
+COOPER='192.168.0.31'
+REMOTE_FOLDER="$COOPER:/data/CooperMedia"
 
-echo $cmd 
-#192.168.0.31:/data/CooperMedia 
+RSYNC_OPTIONS='-av --progress --ignore-errors --delete'
+## Dry run
+RSYNC_OPTIONS="$RSYNC_OPTIONS --dry-run"
+
+RSYNC_EXCLUDE="--exclude '.Trashes'"
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.Spotlight-V100'"
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.DS_Store'"
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '._.DS_Store'"
+    
+## Play Nice with BT Sync
+RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude '.sync'"
+
+
+echo "Backing up Movies"
+# requires sudo if using ssh to transfer data (ip:/local/path)
+eval rsync $RSYNC_OPTIONS $RSYNC_EXCLUDE $LOCAL_FOLDER/TV $REMOTE_FOLDER
 
